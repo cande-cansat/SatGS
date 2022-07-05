0068b11e98f884eaf27303744c070deae3ba2ea7
YEGU KWON <wt2080@gmail.com>
2022-07-05 14:31:39 +0900

    이미지 파일 경로 class

---
+++ SatGS/Model/ImagePath.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SatGS.Model
+{
+    internal struct ImagePath
+    {
+        public string FileName
+        {
+            get
+            {
+                return FullPath.Split('\\').Last();
+            }
+        }
+        public string FullPath { get; }
+        public ImagePath(string path)
+        {
+            FullPath = path;
+        }
+    }
+}
