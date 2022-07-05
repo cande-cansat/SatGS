c002724be41f4bc5b47dc312830f400949c25371
YEGU KWON <wt2080@gmail.com>
2022-07-05 14:33:13 +0900

    위성으로부터 받은 데이터를 처리하는 Factory 생성

--- SatGS/Factory/SatliteStatusFactory.cs
+++ SatGS/Factory/SatliteStatusFactory.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SatGS.Socket;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,40 +9,40 @@ namespace SatGS.Factory
 {
     internal static class SatliteStatusFactory
     {
-        public static Model.SatliteStatus Create(byte[] packet)
+        public static Model.SatliteStatus Create(PacketData packet)
         {
-            int offset = 0;
+            int offset = 1;
             const int step = 4;
 
             Model.Vec3 position = new Model.Vec3();
 
             // latitude
-            position.X = BitConverter.ToSingle(packet, offset); offset += step;
+            position.X = BitConverter.ToSingle(packet.Data, offset); offset += step;
             // longitude
-            position.Y = BitConverter.ToSingle(packet, offset); offset += step;
+            position.Y = BitConverter.ToSingle(packet.Data, offset); offset += step;
             // altitude
-            position.Z = BitConverter.ToSingle(packet, offset); offset += step;
+            position.Z = BitConverter.ToSingle(packet.Data, offset); offset += step;
 
             float heading;
             Model.Vec3 rotation = new Model.Vec3();
             
-            heading = BitConverter.ToSingle(packet, offset); offset += step;
+            heading = BitConverter.ToSingle(packet.Data, offset); offset += step;
             // pitch
-            rotation.Y = BitConverter.ToSingle(packet, offset); offset += step;
+            rotation.Y = BitConverter.ToSingle(packet.Data, offset); offset += step;
             // roll
-            rotation.X = BitConverter.ToSingle(packet, offset); offset += step;
+            rotation.X = BitConverter.ToSingle(packet.Data, offset); offset += step;
             // yaw
-            rotation.Z = BitConverter.ToSingle(packet, offset); offset += step;
+            rotation.Z = BitConverter.ToSingle(packet.Data, offset); offset += step;
 
             Model.Vec3 acceleration = new Model.Vec3();
 
-            acceleration.X = BitConverter.ToSingle(packet, offset); offset += step;
-            acceleration.Y = BitConverter.ToSingle(packet, offset); offset += step;
-            acceleration.Z = BitConverter.ToSingle(packet, offset); offset += step;
+            acceleration.X = BitConverter.ToSingle(packet.Data, offset); offset += step;
+            acceleration.Y = BitConverter.ToSingle(packet.Data, offset); offset += step;
+            acceleration.Z = BitConverter.ToSingle(packet.Data, offset); offset += step;
 
             float temperature, humidity;
-            temperature = BitConverter.ToSingle(packet, offset); offset += step;
-            humidity = BitConverter.ToSingle(packet, offset); offset += step;
+            temperature = BitConverter.ToSingle(packet.Data, offset); offset += step;
+            humidity = BitConverter.ToSingle(packet.Data, offset); offset += step;
 
             return new Model.SatliteStatus
             {
