91c3e93763f1dc804154dc5849b97b5e6f96dfd7
YEGU KWON <wt2080@gmail.com>
2022-07-05 14:33:38 +0900

    Xceed.Wpf 라이센스 검증 코드

--- SatGS/App.xaml.cs
+++ SatGS/App.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
@@ -13,5 +14,9 @@ namespace SatGS
     /// </summary>
     public partial class App : Application
     {
+        public App()
+        {
+            LicenseChecker.CheckLicense();
+        }
     }
 }
---
+++ SatGS/LicenseChecker.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace SatGS
+{
+    internal static class LicenseChecker
+    {
+        private static string GetXceedLicenseKey()
+        {
+            string[] licenseFiles =
+            {
+                "XceedLicense.cfg",
+                "../../XceedLicense.cfg"
+            };
+
+            string LicenseKey = "";
+            foreach (var licenseFile in licenseFiles)
+            {
+                if (!File.Exists(licenseFile)) continue;
+                using (var file = new StreamReader(File.OpenRead(licenseFile)))
+                {
+                    LicenseKey = file.ReadLine();
+                }
+            }
+
+            if (string.IsNullOrEmpty(LicenseKey))
+            {
+                MessageBox.Show("There is no license file for Xceed.Wpf (XceedLicense.cfg)");
+                Environment.Exit(-1);
+            }
+
+            return LicenseKey;
+        }
+
+        private static void CheckXceedLicense()
+        {
+            Xceed.Wpf.Toolkit.Licenser.LicenseKey = GetXceedLicenseKey();
+            Xceed.Wpf.Toolkit.LicenseChecker.CheckLicense();
+        }
+
+        public static void CheckLicense()
+        {
+            CheckXceedLicense();
+        }
+    }
+}
