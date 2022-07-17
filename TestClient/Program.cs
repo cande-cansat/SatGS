c963938fc29589f1d495b82d0ccb434a142dbb64
YEGU KWON <wt2080@gmail.com>
2022-07-17 18:15:19 +0900

    솔루션 정보 업데이트

--- TestClient/Program.cs
+++ TestClient/Program.cs
@@ -78,7 +78,7 @@ namespace TestClient
 
                 for(int y = 0; y < 3; ++y)
                 {
-                    for(int x = 0; x < 4; ++x)
+                    for(int x = 0; x < 4; ++x)  
                     {
                         buffer[offset++] = rotation[y][x];
                     }
