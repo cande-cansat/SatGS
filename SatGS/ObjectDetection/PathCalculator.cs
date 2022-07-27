3a6582b74e50e60e6eb628aa4eefdee0468b22e3
YEGU KWON <wt2080@gmail.com>
2022-07-28 04:26:26 +0900

    이미지 내 물제의 진행방향 계산 함수 추가

---
+++ SatGS/ObjectDetection/PathCalculator.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SatGS.ObjectDetection
+{
+    class PathCalculator
+    {
+        private PositionData oldPos;
+        private PositionData newPos;
+        private float timeDifference;
+
+        public PathCalculator() { }
+
+
+        public void setInitialPos(PositionData newPos)
+        {
+            this.newPos = newPos;
+        }
+        public void setAfterPos(PositionData newPos, float timeDifference)
+        {
+            this.oldPos = this.newPos;
+            this.newPos = newPos;
+            this.timeDifference = timeDifference;
+        }
+
+        public List<Coordinate> calcPath()
+        {
+            oldPos.calcAbsolutePos();
+            newPos.calcAbsolutePos();
+
+            if (newPos.getAbsoluteTargetCartesianPos().item3 <= 0)
+            {
+                return null;
+            }
+
+            Coordinate targetSpeed = Coordinate.realNumMul(Coordinate.vectorSum(newPos.getAbsoluteTargetCartesianPos(), Coordinate.realNumMul(oldPos.getAbsoluteTargetCartesianPos(), -1)), 1/timeDifference);
+            float eta = newPos.getAbsoluteTargetCartesianPos().item3 / ((-1)*targetSpeed.item3);
+
+            List<Coordinate> path = new List<Coordinate>();
+            path.Add(newPos.getAbsoluteTargetCartesianPos());
+            for(float i=1; i<eta; i = i + 1)
+            {
+                path.Add(Coordinate.vectorSum(path[path.Count - 1], targetSpeed));
+            }
+
+            return path;
+        }
+    }
+}
